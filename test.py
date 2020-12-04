da274bb79aa16396d159b5f217ed85d5a60b711c
Jeff0115 <562062907@qq.com>
2020-12-04 15:29:13 +0800

    dsfa

--- test.py
+++ test.py
@@ -24,39 +24,39 @@ FLAGS(['main.py'])
 
 
 def test_model(agent):
-    map_name='MoveToBeacon'
-    envs_num=1
-    max_windows=1
-    total_updates=-1
-    env_args = dict(
-        map_name=map_name,
-        battle_net_map=False,
-        players=[sc2_env.Agent(sc2_env.Race.terran)],
-        agent_interface_format=sc2_env.parse_agent_interface_format(
-            feature_screen=32,
-            feature_minimap=32,
-            rgb_screen=None,
-            rgb_minimap=None,
-            action_space=None,
-            use_feature_units=False,
-            use_raw_units=False),
-        step_mul=8,
-        game_steps_per_episode=None,
-        disable_fog=False,
-        visualize=False
-    )
-    vis_env_args = env_args.copy()
-    vis_env_args['visualize'] = True
-    num_vis = min(envs_num, max_windows)
-    env_fns = [partial(make_sc2env, **vis_env_args)] * num_vis
-    num_no_vis = envs_num - num_vis
-    if num_no_vis > 0:
-      env_fns.extend([partial(make_sc2env, **env_args)] * num_no_vis)
-    envs = SubprocVecEnv(env_fns)
-    # 一个随机的实现方式 用来debug
-    processor=pro(envs.observation_spec()[0])
-    #try:
-
+    # map_name='MoveToBeacon'
+    # envs_num=1
+    # max_windows=1
+    # total_updates=-1
+    # env_args = dict(
+    #     map_name=map_name,
+    #     battle_net_map=False,
+    #     players=[sc2_env.Agent(sc2_env.Race.terran)],
+    #     agent_interface_format=sc2_env.parse_agent_interface_format(
+    #         feature_screen=32,
+    #         feature_minimap=32,
+    #         rgb_screen=None,
+    #         rgb_minimap=None,
+    #         action_space=None,
+    #         use_feature_units=False,
+    #         use_raw_units=False),
+    #     step_mul=8,
+    #     game_steps_per_episode=None,
+    #     disable_fog=False,
+    #     visualize=False
+    # )
+    # vis_env_args = env_args.copy()
+    # vis_env_args['visualize'] = True
+    # num_vis = min(envs_num, max_windows)
+    # env_fns = [partial(make_sc2env, **vis_env_args)] * num_vis
+    # num_no_vis = envs_num - num_vis
+    # if num_no_vis > 0:
+    #   env_fns.extend([partial(make_sc2env, **env_args)] * num_no_vis)
+    # envs = SubprocVecEnv(env_fns)
+    # # 一个随机的实现方式 用来debug
+    # processor=pro(envs.observation_spec()[0])
+    # #try:
+    agent.reset()
     t=0
     max_score=0
     mean_score=0
